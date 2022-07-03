ac11ad501bb18d6535efcd5521d56c694c6d9265
Ludwig Füchsl <ludwig@fuechsl.org>
2022-07-03 12:50:06 +0200

    Video 4

--- Source/TimePower/TimeManager.cpp
+++ Source/TimePower/TimeManager.cpp
@@ -7,8 +7,24 @@
 ATimeManager::ATimeManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
+}
+
+void ATimeManager::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+    
+	// Store recorded time
+	if (TimeReversalAbilityEnabled)
+    {
+        CurrentRecordedTime = TMathUtil<float>::Min(ReverseTimeMaximum, CurrentRecordedTime + DeltaSeconds * CurrentTimeFactor);
+        if (CurrentRecordedTime <= 0.0f)
+        {
+            EndTimeReverse();
+            CurrentRecordedTime = 0.0f;
+        }
+	}
 }
 
 float ATimeManager::GetTimeFactor()
@@ -16,14 +32,45 @@ float ATimeManager::GetTimeFactor()
 	return CurrentTimeFactor;
 }
 
+bool ATimeManager::GetAbilityAvailible()
+{
+	return TimeReversalAbilityEnabled && CurrentRecordedTime >= ReverseTimeThreshold;
+}
+
+float ATimeManager::GetCurrentReversableTime()
+{
+	return CurrentRecordedTime;
+}
+
 void ATimeManager::BeginTimeReverse()
 {
-	CurrentTimeFactor = ReverseTimeFactor;
+	if (GetAbilityAvailible())
+	{
+		CurrentTimeFactor = ReverseTimeFactor;
+	}
 }
 
 void ATimeManager::EndTimeReverse()
 {
 	CurrentTimeFactor = NormalTimeFactor;
+}
+
+void ATimeManager::EnableTimeReverseAbility()
+{
+	if (!TimeReversalAbilityEnabled)
+	{
+		TimeReversalAbilityEnabled = true;
+	}
+}
+
+void ATimeManager::DisableTimeReverseAbility()
+{
+	if (TimeReversalAbilityEnabled)
+	{
+		EndTimeReverse();
+		CurrentRecordedTime = 0.0f;
+		TimeReversalAbilityEnabled = false;
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/TimePower/TimeManager.h
+++ Source/TimePower/TimeManager.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "MathUtil.h"
 #include "GameFramework/Actor.h"
+
 #include "TimeManager.generated.h"
 
 UCLASS()
@@ -15,8 +17,18 @@ class TIMEPOWER_API ATimeManager : public AActor
 		// Sets default values for this actor's properties
 		ATimeManager();
 
+		// Actor tick
+		void Tick(float DeltaSeconds) override;
+
+	public:
+		// Helper getters
 		UFUNCTION(BlueprintCallable)
 		float GetTimeFactor();
+		UFUNCTION(BlueprintCallable)
+		bool GetAbilityAvailible();
+		UFUNCTION(BlueprintCallable)
+		float GetCurrentReversableTime();
+
 
 		// Time reverse control
 		UFUNCTION(BlueprintCallable)
@@ -24,18 +36,36 @@ class TIMEPOWER_API ATimeManager : public AActor
 		UFUNCTION(BlueprintCallable)
 		void EndTimeReverse();
 
+		// Enable control
+		UFUNCTION(BlueprintCallable)
+		void EnableTimeReverseAbility();
+		UFUNCTION(BlueprintCallable)
+		void DisableTimeReverseAbility();
+
 	protected:
 		// Called when the game starts or when spawned
 		virtual void BeginPlay() override;
 
-		// This is the current active factor of time
+	protected:
+		// Current state of ability
 		UPROPERTY(BlueprintReadOnly)
 		float CurrentTimeFactor = 1.0f;
+		UPROPERTY(BlueprintReadOnly)
+		float CurrentRecordedTime = 0.0f;
+
+		// State helpers
+		UPROPERTY(BlueprintReadOnly)
+		bool TimeReversalAbilityEnabled = false;
 
 	public:
-		// Magic time values
+		// Global ability preset
 		UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		float NormalTimeFactor = 1.0f;
 		UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		float ReverseTimeFactor = -3.0f;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float ReverseTimeThreshold = 3.0f;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float ReverseTimeMaximum = 15.0f;
+
 };
